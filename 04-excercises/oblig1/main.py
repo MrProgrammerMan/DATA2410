a8d243b0b0d349f3e0b430449af5bae72ead2057
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-16 08:35:46 +0100

    feat(oblig 1): empty file handling

--- 04-excercises/oblig1/main.py
+++ 04-excercises/oblig1/main.py
@@ -1,7 +1,11 @@
 def read_students(file_path):
 	try:
 		f = open(file_path).read()
-		return f.split("\n")
+		if not f:
+			print(f"File {file_path} is empty")
+			return
+		lines = f.split("\n")
+		return lines
 	except FileNotFoundError:
 		print(f"File {file_path} not found")
 
