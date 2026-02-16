87db890dab9e2e987e0cd8c77d45cd8ceab614e3
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-16 09:58:13 +0100

    feat(oblig 1): invalid line logging

--- 04-excercises/oblig1/main.py
+++ 04-excercises/oblig1/main.py
@@ -1,7 +1,7 @@
 def read_students(file_path):
 	try:
 		f = open(file_path).read()
-		if not f:
+		if len(f.strip()) == 0:
 			print(f"File {file_path} is empty")
 			return
 		lines = f.split("\n")
@@ -10,11 +10,21 @@ def read_students(file_path):
 		print(f"File {file_path} not found")
 
 def process_students(lines):
-	lines_split = map(lambda s: s.split(','), lines)
-	lines_correct_length = filter(lambda l: len(l) == 3, lines_split)
-	lines_valid = filter(lambda l: l[2].isdigit(), lines_correct_length)
+	lines_split = list(map(lambda s: s.split(','), lines))
+
+	l_valid = lambda l: len(l) == 3	and l[2].isdigit() # check for correct length and 3rd item number
+
+	lines_invalid = list(filter(lambda l: not l_valid(l) and list_has_non_whitespace(l), lines_split)) # the second check makes the reject output cleaner
+	lines_valid = filter(l_valid, lines_split)
+
+	if lines_invalid:
+		print(f"Rejected the following invalid line(s): {lines_invalid}")
+
 	lines_tuples = list(map(lambda l: (l[0], l[1], int(l[2])), lines_valid))
 	return lines_tuples
+
+def list_has_non_whitespace(list):
+	return any(len(s.strip()) > 0 for s in list)
 
 
 print(process_students(read_students("input.txt")))
