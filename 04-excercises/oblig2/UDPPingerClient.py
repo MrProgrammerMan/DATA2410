8b25c9bf77e494ef477788620d80c7052a2f3ae3
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-03 19:43:31 +0200

    feat(oblig 2): generate rtt and packet loss report

--- 04-excercises/oblig2/UDPPingerClient.py
+++ 04-excercises/oblig2/UDPPingerClient.py
@@ -8,6 +8,8 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 clientSocket.settimeout(1.0)
 
+rtts = []
+
 try:
     for i in range(1, 11):
         try:
@@ -21,6 +23,7 @@ try:
             after = time.time()
 
             rtt = after-before
+            rtts.append(rtt)
 
             print(f"Response: {message}, RTT: {rtt*1000:.3f}ms")
         except timeout:
@@ -30,3 +33,9 @@ except KeyboardInterrupt:
     print("\nClient shutting down...")
     clientSocket.close()
     sys.exit(0)
+
+print("\n=== REPORT ===")
+print(f"Minimum RTT: {min(rtts)*1000:.3f}ms")
+print(f"Maximum RTT: {max(rtts)*1000:.3f}ms")
+print(f"Average RTT: {sum(rtts)*1000 / len(rtts):.3f}ms")
+print(f"Packet loss: {(10 - len(rtts))*10}%")
