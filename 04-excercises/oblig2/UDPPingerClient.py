84bb895561422069f9fa67c20d7755fc1a7ec79e
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-03 19:56:28 +0200

    feat(oblig 2): improve output to match task

--- 04-excercises/oblig2/UDPPingerClient.py
+++ 04-excercises/oblig2/UDPPingerClient.py
@@ -10,6 +10,8 @@ clientSocket.settimeout(1.0)
 
 rtts = []
 
+print("Pinging server at 127.0.0.1:12000...\n")
+
 try:
     for i in range(1, 11):
         try:
@@ -18,24 +20,22 @@ try:
             before = time.time()
 
             clientSocket.sendto(message.encode("utf-8"), ("127.0.0.1", 12000))
-            message, _ = clientSocket.recvfrom(1024)
+            clientSocket.recvfrom(1024)
 
             after = time.time()
 
             rtt = after-before
             rtts.append(rtt)
 
-            print(f"Response: {message}, RTT: {rtt*1000:.3f}ms")
+            print(f"Packet {i}: RTT = {rtt*1000:.3f}ms ✓")
         except timeout:
-            print("Request timed out")
+            print(f"Packet {i}: Request timed out ✗")
             continue
 except KeyboardInterrupt:
     print("\nClient shutting down...")
     clientSocket.close()
     sys.exit(0)
 
-print("\n=== REPORT ===")
-print(f"Minimum RTT: {min(rtts)*1000:.3f}ms")
-print(f"Maximum RTT: {max(rtts)*1000:.3f}ms")
-print(f"Average RTT: {sum(rtts)*1000 / len(rtts):.3f}ms")
-print(f"Packet loss: {(10 - len(rtts))*10}%")
+print("\n--- Statistics ---")
+print(f"10 packets sent, {len(rtts)} received, {(10 - len(rtts))*10}% packet loss")
+print(f"Minimum RTT = {min(rtts)*1000:.3f}ms, Maximum RTT: {max(rtts)*1000:.3f}ms, Average RTT: {sum(rtts)*1000 / len(rtts):.3f}ms")
